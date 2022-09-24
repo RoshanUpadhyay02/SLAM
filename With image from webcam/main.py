dd0209898b8911bf3c85cce8ff3bf6c395d9659b
Roshan Upadhyay <roshanupadhyay.projects@gmail.com>
2022-09-24 16:51:25 +0530

    update code

--- With image from webcam/main.py
+++ With image from webcam/main.py
@@ -4,13 +4,10 @@ import env
 from robots import Mouse, Roomba
 
 environment = env.BuildEnvironment()
-
 running = True
-
 placed = False
 
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -19,17 +16,13 @@ while running:
             placed = True
         if not placed:
             continue
-
     if not pygame.mouse.get_focused():
         continue
 
-    
     environment.iterate()
     environment.render_map()
     environment.update()
-    
     pygame.display.update()
-
 
 environment.save()
 pygame.quit()
